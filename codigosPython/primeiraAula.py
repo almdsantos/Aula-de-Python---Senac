e949a64dad5aff7c083b40f19f0e5bb88088acf6
almdsantos <aliston.m.santos@outlook.com.br>
2023-08-30 13:58:12 -0300

    first commit

--- codigosPython/primeiraAula.py
+++ codigosPython/primeiraAula.py
@@ -14,3 +14,15 @@ valor = 1500.00
 print("Curso: ", curso, " Custa: R$ ", valor, "em ", ano)
 
 #--------------------------------------------------------------------------
+
+codigo = 123
+nome = "Aliston Santos"
+ativo = True
+salario = 2346.15
+
+print("Código: %d \t\t tipo: %s" % (codigo, type(codigo)))
+print("Nome: %s \t\t tipo: %s" % (nome, type(nome)))
+print("Ativo %r \t\t tipo: %s" % (ativo, type(ativo)))
+print("Salário: %.2f \t\t tipo: %s" % (salario, type(salario)))
+
+#--------------------------------------------------------------------------
